061bb112d50812c797c7194858128bf50549bd82
Philipp Häfelfinger <philipp@haefelfinger.ch>
2019-03-21 22:31:16 +0100

    formatting

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,12 +8,12 @@ import (
 )
 
 var (
-	imagesRootPath            = flag.String("imagesRootPath", "", "This is the images root path that should be mirrored to piwigo.")
-	sqliteDb                  = flag.String("sqliteDb", "./localstate.db", "The connection string to the sql lite database file.")
-	noUpload                  = flag.Bool("noUpload", false, "If set to true, the metadata gets prepared but the upload is not called and the application is exited with code 90")
-	piwigoUrl                 = flag.String("piwigoUrl", "", "The root url without tailing slash to your piwigo installation.")
-	piwigoUser                = flag.String("piwigoUser", "", "The username to use during sync.")
-	piwigoPassword            = flag.String("piwigoPassword", "", "This is password to the given username.")
+	imagesRootPath = flag.String("imagesRootPath", "", "This is the images root path that should be mirrored to piwigo.")
+	sqliteDb       = flag.String("sqliteDb", "./localstate.db", "The connection string to the sql lite database file.")
+	noUpload       = flag.Bool("noUpload", false, "If set to true, the metadata gets prepared but the upload is not called and the application is exited with code 90")
+	piwigoUrl      = flag.String("piwigoUrl", "", "The root url without tailing slash to your piwigo installation.")
+	piwigoUser     = flag.String("piwigoUser", "", "The username to use during sync.")
+	piwigoPassword = flag.String("piwigoPassword", "", "This is password to the given username.")
 )
 
 func Run() {
