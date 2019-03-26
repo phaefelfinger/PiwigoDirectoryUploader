582b0621c29e6d5d981c0b1750e4173aafd499a1
Philipp Häfelfinger <philipp@haefelfinger.ch>
2019-03-27 00:28:07 +0100

    sorted image list that will be uploaded

--- internal/app/datastore.go
+++ internal/app/datastore.go
@@ -168,7 +168,7 @@ func (d *localDataStore) ImageMetadataToUpload() ([]ImageMetaData, error) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT imageId, piwigoId, fullImagePath, fileName, md5sum, lastChanged, categoryPath, categoryId, uploadRequired, deleteRequired FROM image WHERE uploadRequired = 1 and deleteRequired = 0")
+	rows, err := db.Query("SELECT imageId, piwigoId, fullImagePath, fileName, md5sum, lastChanged, categoryPath, categoryId, uploadRequired, deleteRequired FROM image WHERE uploadRequired = 1 and deleteRequired = 0 order by fullImagePath asc")
 	if err != nil {
 		return nil, err
 	}
